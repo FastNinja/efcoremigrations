433bf9c6378d1c510169adc0d7665b6955f7488d
FastNinja <alex.maslyukov@myob.com>
2017-08-23 15:57:19 +1000

    added docker compose to run postregss DB tests inside docker

--- eftests/UnitTest1.cs
+++ eftests/UnitTest1.cs
@@ -8,8 +8,14 @@ namespace eftests
         [Fact]
         public void LetsCreateADatabase_ShallWe()
         {
-                var ctx = new BloggingContext("Host=localhost;Database=eftry;Username=postgres;Password=postgres");
-                ctx.MigrateSchema();
+            var connextionString = Environment.GetEnvironmentVariable("POSTGRES_CONNECTION_STRING");
+            if(string.IsNullOrEmpty(connextionString)){
+                throw new Exception("missing posgress connection string in env POSTGRES_CONNECTION_STRING " );
+            }
+            Console.WriteLine(connextionString);
+
+            var ctx = new BloggingContext(connextionString);
+            ctx.MigrateSchema();
 
         }
     }
